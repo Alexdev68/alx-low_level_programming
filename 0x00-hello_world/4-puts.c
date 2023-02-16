6fe3f1bf07c580cd015a351be18f376f26825aca
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-16 06:14:19 -0800

    Ok, this stuff needs to be perfect to work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-/* code to print out a certain line*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char sentence[] = "\"Programming is like building a miltiligual puzzle";
+	char sentence[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(sentence);
+
 	return (0);
-}	
+}
