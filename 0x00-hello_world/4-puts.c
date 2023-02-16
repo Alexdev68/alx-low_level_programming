83f0a776a76f3f8f5b0fc24a97e8fac2fbc36f68
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-16 05:59:36 -0800

    :(

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main()
+/* code to print out a certain line*/
+int main(void)
 {
-	char sentence[] = "Programming is like building a miltiligual puzzel";
-
+	char sentence[] = "\"Programming is like building a miltiligual puzzle";
 	puts(sentence);
-	
-	return 0;
+	return (0);
 }	
