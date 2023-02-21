d0d10a88c44db8a06b60b0a7a72972d4782c6941
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-21 08:02:46 -0800

    Nice

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 'z'; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
