5228a3158ae3b586fc505e47a4d43e4c4e179d30
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-21 14:29:48 -0800

    Life

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @c: integer
+ * Return: 1
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_call = c * -1;
+		return (abs_call);
+	}
+	return (c);
+}
