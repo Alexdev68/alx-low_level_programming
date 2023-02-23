bdfe2718eba9c69ef1f8ab2f901b744b7d62ec63
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-23 03:16:31 -0800

    New day

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Returns 1 if uppercase then 0 if lowercase
+ * @c: Integer
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'B')
+	{
+		return (1);
+	}
+	return (0);
+}
