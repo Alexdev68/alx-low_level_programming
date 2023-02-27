913d5fe2883576276b09be73d3b955f99f38f4cd
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-27 08:00:44 -0800

    :)

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - This swaps the values of two integers
+ *
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
