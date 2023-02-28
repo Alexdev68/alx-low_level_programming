703d32a59b17bae707c86bce28c6d168cdfe69d0
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-02-27 17:35:15 -0800

    Gone

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcpy - This copies the string pointed to by src
+ * @dest: Destination of copied string
+ * @src: Source of copied string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, a;
+
+	while (*src)
+	{
+		src++;
+		len++;
+	}
+	for (a = 0; a < len; a++)
+	{
+		src--;
+	}
+	for (a = 0; a < len; a++)
+	{
+		dest[a] = *src;
+		src++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
