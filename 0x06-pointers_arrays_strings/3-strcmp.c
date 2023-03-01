4d3c6ffd755c1c27cc7a4cdf6d7563d8dd67fdb5
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-01 05:41:13 -0800

    Sweetness

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - This compares two strings
+ * @s1: This the first string
+ * @s2: This the second string
+ * Return: result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+
+	return (res);
+}
