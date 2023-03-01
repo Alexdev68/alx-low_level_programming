f36d5eb483c98afc4b5c0a812661f13a48123f4e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-01 12:13:41 -0800

    Lovely code

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - This changes all lowercase characters in an array to uppercase
+ * @str: pointer
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i]=(int)str[i] - 32;
+		}
+	}
+	return (str);
+}
