48b6fb3854e10aa57ddce5afd26ec07ffedd7909
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-06 06:29:13 -0800

    Tasks 0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_memset - This fills a memory with a constant byte
+ * @s: Pointer
+ * @b: This is the constant byte
+ * @n: This is the number of bytes that should be filled
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	if (s == NULL) return (NULL);
+
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+
+	return (s);
+}
