1058157eb0e267f7044dc82625012d3e9e8073ed
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-13 10:20:36 -0700

    Task 1 (0x0B. C - malloc, free)

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory which
+ * contain a copy of the string
+ * @str: This the string pointer
+ * Return: returns a pointer to the duplicated string, and NULL if insufficent
+ * memory was available
+ */
+char *_strdup(char *str)
+{
+	int size;
+	char *dup;
+	char *duplication;
+
+	size = strlen(str);
+	dup = malloc(sizeof(char) * size);
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	duplication = dup;
+	while (*str)
+	{
+		*duplication = *str;
+		duplication++;
+		str++;
+	}
+	*duplication = '\0';
+	return (dup);
+}
