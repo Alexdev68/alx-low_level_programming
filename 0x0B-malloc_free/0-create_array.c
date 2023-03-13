e1eb9bb4812f1752d8fafe5d7f41b7955b8a78ae
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-13 08:53:37 -0700

    Task 0 (0x0B. C - malloc, free)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - This creates an array of chars
+ * @size: This is the size desired
+ * @c: This is the character to be used to initialize the array
+ * Return: returns a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is my introduction to malloc, free Readme file
