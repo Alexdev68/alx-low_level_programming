6c2d901faf5f351c567233241a3722627e55468a
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-13 12:07:19 -0700

    Task 2 (0x0B. C - malloc, free)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,15 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	char *concat;
 
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+
 	size = strlen(s1) + strlen(s2);
 	cat = malloc(sizeof(char) * size + 1);
 
