ddc314277b094a3036c277546340097faa8e5636
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-16 15:57:13 -0700

    Task 1 (0x0C. C - More malloc, free)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,16 +12,14 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, k, len_2, len;
+	unsigned int i, k, len_2 = 0, len = 0;
 
-	len_2 = 0;
-	len = 0;
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 	{
 		len++;
 	}
 
-	for (k = 0; s2[k] != '\0' && k < n; k++)
+	for (k = 0; s2[k] && k < n; k++)
 	{
 		len_2++;
 	}
