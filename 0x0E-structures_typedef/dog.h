80baf240de50629dab02bbf0f55d2752985a417d
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-20 13:35:57 -0700

    Task 0 (0x0E. C - Structures, typedef)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - This is a profile of a dog consisting of (name, age and owner)
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
 struct dog
 {
 	char *name;
