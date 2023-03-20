725c1af25419e2aed32fc626ecb94b10822d059c
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-20 16:55:00 -0700

    Task 4 (0x0E. C - Structures, typedef)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * *new_dog - This creates a new dog
@@ -11,7 +12,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nd;
+	char len1, len2;
+	char *name_mem, *owner_mem;
+	int i;
 
+	len1 = strlen(name);
+	len2 = strlen(owner);
 	nd = malloc(sizeof(dog_t));
 
 	if (nd == NULL)
@@ -19,9 +25,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	nd->name = name;
+	name_mem = malloc(sizeof(char) * len1 + 1);
+
+	if (name_mem == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		name_mem[i] = name[i];
+	}
+	name_mem[i] = '\0';
+
+	owner_mem = malloc(sizeof(char) * len2 + 1);
+
+	if (owner_mem == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+
+	for (i = 0; i < len2; i++)
+	{
+		owner_mem[i] = owner[i];
+	}
+	owner_mem[i] = '\0';
+
+	nd->name = name_mem;
 	nd->age = age;
-	nd->owner = owner;
+	nd->owner = owner_mem;
 
 	return (nd);
 }
