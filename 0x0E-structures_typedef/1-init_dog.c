b5593e0cc6d98384ee03890dd6e07850ba773357
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-20 14:05:55 -0700

    Task 1 (0x0E. C - Structures, typedef)

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - This initializes a variable of type struct dog
+ * @d: variable being initialized
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
