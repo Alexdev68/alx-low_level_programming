0e596a523c1bee1a86fa6a49de983ce9d4baa5f9
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-22 12:46:14 -0700

    Task 2 (0x0F. C - Function pointers)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,6 +17,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
+	if (cmp == NULL || array == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
