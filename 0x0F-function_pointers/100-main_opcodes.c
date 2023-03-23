498149a1e46656bb64fc85dd7d2f263f6728cdfe
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-22 19:16:28 -0700

    Task 4 (0x0F. C - Function pointers)

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This function prints it's own opcodes
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int number_of_bytes, i;
+	char *main_ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	main_ptr = (char *) main;
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%2hhx ", main_ptr[i]);
+	}
+	putchar('\n');
+	return (0);
+}
