386c6272fad518c806bf71689daf42119db96192
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-23 14:51:54 -0700

    Task 3 (0x10. C - Variadic functions)

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,7 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0, n = strlen(format);
+	char *g, *separator = ", ";
 
 	va_list args;
 
@@ -19,30 +20,37 @@ void print_all(const char * const format, ...)
 
 	while (i < n)
 	{
-		if (format[i] == 'c')
+		if (format[i])
 		{
-			int g = va_arg(args, int);
-			printf("%c, ", g);
-		}
-		else if (format[i] == 'i')
-		{
-			int g = va_arg(args, int);
-			printf("%d, ", g);
-		}
-		else if (format[i] == 'f')
-		{
-			double g = va_arg(args, double);
-			printf("%f, ", g);
-		}
-		else if (format[i] == 's')
-		{
-			char *g = va_arg(args, char *);
-			if (g == NULL)
+			switch (format[i])
 			{
-				g = ("(nil)");
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+
+				case 's':
+					g = va_arg(args, char *);
+					if (g == NULL)
+					{
+						g = "(nil)";
+					}
+					printf("%s", g);
+					break;
+
+				default:
+					i++;
+					continue;
 			}
-			printf("%s, ", g);
 		}
+		printf("%s", (i < (n - 1)) ? separator : "");
 		i++;
 	}
 	putchar ('\n');
