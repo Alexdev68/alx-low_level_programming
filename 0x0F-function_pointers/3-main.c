c04d29cb886519e1308f1437146f1608ebf2e7ee
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-22 17:55:10 -0700

    Task 3 (0x0F. C - Function pointers)

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,21 +13,21 @@ int main(int argc, char *argv[])
 	int num1, num2, res;
 	char *op;
 
-	op = argv[2];
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (get_op_func(op) == NULL || argv[2] [1])
+	if (get_op_func(op) == NULL || argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	op = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
