be43de33c262dbd787b0ab95d18ce340af671eeb
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-30 07:28:29 -0700

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-		h = h->next;
-		count++;
-	}
-	while (h)
-	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		count++;
 	}
