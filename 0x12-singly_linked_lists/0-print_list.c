048ee03bedacf7b958e87358ecbf514c09818f74
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-30 06:08:33 -0700

    New project files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_list - This function prints all the elements of a list_t list
+ * @h: This pointer points to the head of the list_t
+ * Return: This returns the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
