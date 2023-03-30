8e332f5d3708564a4c6909e5e96024197b1633bd
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-03-30 14:51:49 -0700

    Fixing errors

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,13 @@
-	SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
 
 	SECTION .text
 	extern printf
 	global main, _start
 main:
-	push rbp
 
-	mov esi, msg	;
 	mov edi, fmt	;
+	xor eax, eax	;
 	mov eax, 0	;
 	call printf
-
-	pop rbp
-
 	mov ebx, 0	;
-	mov eax, 1	;
-	int 0x80	;
+	ret
+fmt: db `Hello, Holberton\n`, 0
