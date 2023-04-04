e637df052d7a46cea9da53a50a44cdac59c01269
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-04 04:49:13 -0700

    Fixing errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -30,29 +29,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		x = x->next;
 		count++;
 	}
-
 	if (idx - 1 > count)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	if (x == NULL && count == idx - 1)
+	if (x == NULL && (count == idx - 1))
 	{
 		new_node->next = NULL;
 	}
-
-	if (idx == 0)
+	if ((count == idx - 1) == 0)
 	{
-		new_node = (*head);
+		new_node->next = (*head);
 		(*head) = new_node;
 	}
-
-	new_node->n = n;
-
-	new_node->next = x->next;
-
-	x->next = new_node;
-
+	if (count == idx - 1)
+	{
+		new_node->n = n;
+		new_node->next = x->next;
+		x->next = new_node;
+	}
 	return (new_node);
 }
