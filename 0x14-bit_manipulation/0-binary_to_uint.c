5a6feba8d7e0ee473e06a4c3ae9d32429d6b1e31
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-07 05:31:10 -0700

    Fixed errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,13 +13,17 @@ unsigned int binary_to_uint(const char *b)
 
 	len = strlen(b);
 
-	if (b == NULL)
+	if (b == NULL || len == 0)
 	{
 		return (0);
 	}
 
 	for (i = (len - 1); i >= 0; i--)
 	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
 		if (b[i] == '1')
 		{
 			tot += calc;
