7af653d7a9a8e2180f6b21081676e0dcc25e6860
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-07 05:42:28 -0700

    Fixing

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 
 	len = strlen(b);
 
-	if (b == NULL || len == 0)
+	if (b == NULL)
 	{
 		return (0);
 	}
@@ -31,10 +31,5 @@ unsigned int binary_to_uint(const char *b)
 		}
 		calc *= 2;
 	}
-	if (tot > (UINT_MAX - calc) / 2)
-	{
-		return (0);
-	}
-
 	return (tot);
 }
