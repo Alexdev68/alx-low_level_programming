9392f77b7bbbd96767f1175149875041b33a5c6e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-08 04:32:07 -0700

    Fixing errors

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,49 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char bit[20];
-	int i, len = 0, x = 0;
-	unsigned int count = 0;
-
-	while (n != 0)
-	{
-		if (n % 2 == 1)
-		{
-			bit[len] = '1';
-		}
-		else
-		{
-			bit[len] = '0';
-		}
-		n /= 2;
-		len++;
-	}
-	bit[len] = '\0';
-	/*
-	for (i = (len - 1); i >= 0; i--, k++)
-	{
-		bin[k] = bit[i];
-	}
-	bin[k] = '\0';*/
-
-	for (i = 0; bit[i] != '\0'; i++)
-	{
-		if (count == index)
-		{
-			if (bit[i] == '1')
-			{
-				x += 1;
-			}
-			else
-			{
-				x += 0;
-			}
-		}
-		count++;
-	}
-	if (count < index)
+	if (index > sizeof(unsigned int long) * 8)
 	{
 		return (-1);
 	}
-	return (x);
+
+	return ((n >> index) & 1);
 }
