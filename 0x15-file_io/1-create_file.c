8408d3692d75efc2f2733dfb493082b7bd76037f
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-09 16:22:17 -0700

    Fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		close(fd);
+		return (-1);
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
