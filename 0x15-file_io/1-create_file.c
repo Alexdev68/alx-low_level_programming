4d467713ae7b8d4d2124fc548033df91cdf8f381
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-09 11:00:33 -0700

    Fixing errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		close(fd);
+		len = 0;
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
