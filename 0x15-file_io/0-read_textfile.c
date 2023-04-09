0aed080a96d01c29590465e8d845c1a5176ce402
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-09 02:23:20 -0700

    New files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - This function reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: This is the file pointer
+ * @letters: This is the number of characters to be read and printed out
+ * Return: This returns the actual number of letters it could read and print
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	char holder;
+	size_t count = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fptr = fopen(filename, "r");
+	
+	while ((holder = getc(fptr)) != EOF)
+	{
+		if (count == letters)
+		{
+			break;
+		}
+		putc(holder, stdout);
+		count++;
+	}
+
+	fclose(fptr);
+
+	return (count);
+}
