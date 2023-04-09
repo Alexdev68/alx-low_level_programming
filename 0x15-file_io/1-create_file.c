1fd6c6103d2f3ad260c940f8cf7b402a93fabe58
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-09 15:17:11 -0700

    Fixing errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,23 +19,21 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		len = 0;
-	}
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		return (-1);
 	}
-
-	i = write(fd, text_content, len);
-
-	if (i == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		i = write(fd, text_content, len);
+
+		if (i == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
