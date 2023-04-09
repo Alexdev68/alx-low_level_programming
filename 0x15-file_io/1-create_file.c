171ce9b2fcc819a24543455ad516bd0c4bb52521
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-04-09 15:46:32 -0700

    Fixing errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t i = 0, len = strlen(text_content);
+	ssize_t i, len = strlen(text_content);
 
 	if (filename == NULL)
 	{
