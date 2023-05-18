8b8093487dab94c8de92606be8419506bb6998cc
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-18 05:36:24 -0700

    Fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 
-
+/**
+ * print_dlistint - This function prints all the data of a linked list
+ * @h: This is a pointer to the head of a linked list
+ * Return: This returns the length of the linked list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
