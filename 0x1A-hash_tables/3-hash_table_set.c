6847ec93b6472de29e8d09c24ac09349beb1be0e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 11:50:33 -0700

    Debugging

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,6 +6,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	int hash;
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
@@ -18,10 +19,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
+	hash = hash_djb2((const unsigned char *)key);
+
 	new->key = strdup(key);
 	new->value = strdup(value);
 
-	ht->array[0] = new;
+	ht->array[hash] = new;
 
 	return (1);
 }
