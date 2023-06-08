a54779942827bd89fa10877c94fc10c87de23cf2
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 10:57:57 -0700

    Fixed betty code style error

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,12 @@
 #include "hash_tables.h"
 #include <stddef.h>
 
-
+/**
+ * key_index - This function gets the index of the key entered
+ * @key: This is the key whose index we are checking
+ * @size: This is the array size
+ * Return: This returns the index of the key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
@@ -10,5 +15,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	hash = hash_djb2(key);
 	index = hash % size;
 
-	return index;
+	return (index);
 }
