dfaded54c63a49028ac0254fb503f3c87c7ef371
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 09:25:28 -0700

    New files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * *hash_table_create - This function creates a hash table
+ * *size: This is the size of the array
+ * Returns: This returns the new table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+
+	hash_table_t *ntable;
+
+	ntable = malloc(sizeof(hash_table_t));
+	if (ntable == NULL)
+	{
+		return (NULL);
+	}
+
+	ntable->size = size;
+
+	ntable->array = calloc(ntable->size, sizeof(hash_node_t*));
+
+	for (i = 0; i < ntable->size; i++)
+	{
+		ntable->array[i] = NULL;
+	}
+
+	return (ntable);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+# 0x1A. C - Hash tables
+This is a whole new way of storing that the Alx SE program has tasked us with a variety of tasks and also an introduction.
