a724f15bea0b3faebb826e783b94a932f1d4642c
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 18:35:00 -0700

    Fixing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+/**
+ * hash_table_set - This function inserts an element into the hash table
+ * @ht: This is the hash table
+ * @key: This the key to be inserted
+ * @value: This is the value to be inserted
+ * Return: This returns 1 if it succeeded, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	int hash;
+	int index;
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
@@ -19,12 +25,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	hash = hash_djb2((const unsigned char *)key);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 
-	ht->array[hash] = new;
+	index = hash_djb2((const unsigned char *)key);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+	}
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
 
 	return (1);
 }
