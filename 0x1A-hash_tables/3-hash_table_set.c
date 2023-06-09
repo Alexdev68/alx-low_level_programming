62fbd54d5e62dc4a3f43f9a346ed83efe2f94867
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-09 04:05:41 -0700

    Debugging

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	hash_node_t * current;
+	hash_node_t *current;
 	int index;
 
 	new = malloc(sizeof(hash_node_t));
