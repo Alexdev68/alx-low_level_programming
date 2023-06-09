d317a1faa0f0a32c3357b4b6d42132fac44fd2c4
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-09 10:01:01 -0700

    Fixing unseen issue

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -25,12 +25,19 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf(", ");
 			}
+
 			current = ht->array[i];
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
-			if (current != NULL)
+
+			while (current != NULL)
 			{
-				printf(", ");
+				printf("'%s': '%s'", current->key, current->value);
+
+				current = current->next;
+
+				if (current != NULL)
+				{
+					printf(", ");
+				}
 			}
 			count++;
 		}
