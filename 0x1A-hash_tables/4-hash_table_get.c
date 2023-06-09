9b381b3fd9ecbeb484a9b0b5ef441c8e0b0b1079
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-09 08:30:30 -0700

    Added an extra check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *pair;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 	{
 		return (NULL);
 	}
