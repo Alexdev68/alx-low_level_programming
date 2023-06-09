ffb6ff5f9501cc54bdf066c90ed7c3fa90b8ba6e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-09 03:50:34 -0700

    Debugging

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,6 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->next = ht->array[index];
 	ht->array[index] = new;
+	ht->array[index] = ht->array[index]->next;
 
 	return (1);
 }
