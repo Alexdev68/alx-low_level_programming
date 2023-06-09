bdeda3b48d8065ca52045b384a9aa117c1f39e86
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-09 12:15:33 -0700

    Freed completly

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,15 +9,21 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *pair;
+	hash_node_t *current;
 
 	while (i < ht->size)
 	{
-		pair = ht->array[i];
-		if (pair != NULL)
+		if (ht->array != NULL)
 		{
-			free(pair->key);
-			free(pair->value);
-			free(pair);
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				pair = current;
+				current = current->next;
+				free(pair->key);
+				free(pair->value);
+				free(pair);
+			}
 		}
 		i++;
 	}
